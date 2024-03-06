7d7c133e978bafd09f79764501e472bb4aee135f
Sergeeva Larisa <mindzu@mail.ru>
2024-03-06 12:58:37 +0500

    help for gendiff

--- differencies/scripts/gendiff.py
+++ differencies/scripts/gendiff.py
@@ -1,11 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+'''Compares two configuration files and shows a difference.
+'''
+
+import argparse
 
 
 def main():
-      print('''Тут будет справка
-            дадада''')
+      parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+      parser.add_argument('first_file')
+      parser.add_argument('second_file')
+      args = parser.parse_args()
+      #print(args.first_file, args.second_file)
+      #print('Compares two configuration files and shows a difference.')
 
 
 if __name__ == '__main__':
       main()
-      
