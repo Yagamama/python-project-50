aece653ccd052f148cb491d1c5287e6fc075513e
Sergeeva Larisa <mindzu@mail.ru>
2024-03-06 13:29:06 +0500

    add -f --format to help gendiff

--- differencies/scripts/gendiff.py
+++ differencies/scripts/gendiff.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
-'''Compares two configuration files and shows a difference.
-'''
+
 
 import argparse
 
 
 def main():
-      parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+      parser = argparse.ArgumentParser(
+            description='Compares two configuration files and shows a difference.')
+      parser.add_argument('-f', '--format', help='set format of output')
       parser.add_argument('first_file')
       parser.add_argument('second_file')
       args = parser.parse_args()
-      #print(args.first_file, args.second_file)
-      #print('Compares two configuration files and shows a difference.')
 
 
 if __name__ == '__main__':
